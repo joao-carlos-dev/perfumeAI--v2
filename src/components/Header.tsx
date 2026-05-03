56f89b58710576a0c09bbbd3ba995a1d7c2a708f
joao-carlos-dev <joaocarlosifsc@hotmail.com>
2026-05-03 14:38:49 -0300

    Inicialização do projeto e criação do header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="bg-brand-bg py-6">
+            <div className="max-w-[1126px] mx-auto px-4">
+                <h1 className="text-brand-accent text-2xl font-bold uppercase">Perfume<span className="text-pink-600">AI</span></h1>
+                <h2 className="text-xl font-semibold text-white py-4">Boa descoberta</h2>
+                <p className="text-gray-400" >Suas recomendações personalizadas</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
